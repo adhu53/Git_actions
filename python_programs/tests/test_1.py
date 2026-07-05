414bc6de5f74f4d9f2a8e9564b8dadd5d2a73580
adhu53 <adarshmd53@gmail.com>
2026-07-05 15:34:28 +0530

    worflow

--- python_programs/tests/test_1.py
+++ python_programs/tests/test_1.py
@@ -1,7 +1,7 @@
 from python_programs.src.script import add
 
-def test_1():
-    assert add(a,b)==4
+def test_add_4():
+    assert add(2, 2) == 4
 
-def test_2():
-    assert add(a,b)==5
+def test_add_5():
+    assert add(2, 3) == 5
